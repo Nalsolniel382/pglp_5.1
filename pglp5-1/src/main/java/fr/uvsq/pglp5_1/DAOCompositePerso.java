67024dbc14b5fec8bece34531f61c1b67dd6bd88
Nalsolniel <thlapegue88@gmail.com>
2020-05-09 16:46:52 +0200

    serealisation de composite perso

--- pglp5-1/src/main/java/fr/uvsq/pglp5_1/DAOCompositePerso.java
+++ pglp5-1/src/main/java/fr/uvsq/pglp5_1/DAOCompositePerso.java
@@ -1,6 +1,12 @@
 package fr.uvsq.pglp5_1;
 
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Map;
@@ -15,25 +21,96 @@ public class DAOCompositePerso implements DAOapp<CompositePerso>, Serializable {
 	  public DAOCompositePerso() {
 	        list = new ArrayList<CompositePerso>();
 	    }
-	  
+	  //Sere
 	  public void serialize(final CompositePerso c, final String f) {
+		  
+		  ObjectOutputStream out = null;
+          try {
+            final FileOutputStream fichier = new FileOutputStream(f);
+            out = new ObjectOutputStream(fichier);
+            out.writeObject(c);
+            
+            out.flush();
+            
+          }
+          catch (java.io.IOException e)
+          {
+            e.printStackTrace();
+          }
+          
+          try {
+              out.close();
+              
+          } 
+          catch (IOException e)
+          {
+              e.printStackTrace();
+          }
 	       
 	    }
-	 
-	 
+	 //desere
+	  public CompositePerso deserialize(final String f) {
+	        ObjectInputStream in = null;
+	        CompositePerso ret = null;
+	        try {
+	            final FileInputStream fichier = new FileInputStream(f);
+	            in = new ObjectInputStream(fichier);
+	            ret = (CompositePerso) in.readObject();
+	            fichier.close();
+	        }
+	        catch (java.io.IOException e)
+	        {
+	            e.printStackTrace();
+	        }
+	        catch (ClassNotFoundException e) 
+	        {
+	            e.printStackTrace();
+	        }
+	        return ret;
+	    }
 	  //get 
 	  
-	  public CompositePerso get(final int id) {
-	        for (CompositePerso c : list) {
+	  public CompositePerso get(final int ID) {
+	      /*  for (CompositePerso c : list) {
 	            if (c.getid() != id);
 	            else{ return c;}
 	        }
+	        return null;*/
+		  int i;
+	        
+	        File repertoire = new File("CompositePerso");
+	        String[] liste = repertoire.list();
+	        if (liste.length == 0) {
+	            return null;
+	        } else {
+	            for (i = 0; i < liste.length; i++)
+	            {
+	                if (liste[i].equals(ID))
+	                {
+	                    return this.deserialize("CompositePerso\\" + liste[i]);
+	                }
+	                else {System.out.println("le CompositePerso n'a pas été trouvé");}
+	            }
+	        }
 	        return null;
 	    }
+	  
 	  //add 
 	  public void add(final CompositePerso New) {
-	        list.add(New);
+	       // list.add(New);
+		  int i;
+	        File repertoire = new File("CompositePerso");
+	        String[] liste = repertoire.list();
+	        for (i = 0; i < liste.length; i++) {
+	            if (Integer.parseInt(liste[i]) == New.getid()) {
+	              return;
+	            }
+	        }
+	        this.serialize(New, "CompositePerso\\" + (New.getid()));
 	    }
+	    
+	 
+	  
 	  //up 
 	  public CompositePerso update(final CompositePerso New) {
 		  CompositePerso old = this.get(New.getid() );
